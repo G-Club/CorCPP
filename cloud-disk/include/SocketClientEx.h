d22c2f118e69098a0f4febd7e5a8cdc30a281509
yoyao <fwxshiwo@gmail.com>
2017-10-17 22:27:01 +0800

    added header to prj

---
+++ cloud-disk/include/SocketClientEx.h
@@ -0,0 +1,89 @@
+
+#pragma once
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+
+#include <iostream>
+#include <string>
+
+#ifdef WIN32
+
+#include <WS2tcpip.h>
+#include <winsock2.h>
+
+typedef SOCKET SOCKETex_t;
+
+#define SEX_INVALID_SOCKET INVALID_SOCKET
+#define ioctl ioctlsocket
+#define Errcode GetLastError()
+#define close closesocket
+#define S_PROTO_TCP IPPROTO_TCP
+#define SEX_EINPROGRESS  WSAEWOULDBLOCK
+
+#endif
+
+#ifdef linux
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include<arpa/inet.h>
+
+typedef int SOCKETex_t;
+#define SEX_INVALID_SOCKET -1
+#define Errcode errno
+#define close close
+
+#define S_PROTO_TCP 0
+#define SEX_EINPROGRESS  EINPROGRESS
+
+#endif
+
+enum SEX_ERR_TYPE
+{
+	SEX_NONE_ERR=0,
+	SEX_TIME_OUT = 1,
+	SEX_ERROR=2
+};
+class SocketClientEx
+{
+
+public:
+	SocketClientEx();
+
+	int Init();
+	/*
+	*连接方法，带超时时间
+	*/
+	SEX_ERR_TYPE Connect2(std::string &ip, unsigned short port, unsigned int timeout);
+	/*
+	*断开连接
+	*/
+	SEX_ERR_TYPE DisConnect();
+	/*
+	*接收信息
+	*/
+	int Receive(std::string &msg, unsigned int timeout);
+	/*
+	*接收信息
+	*/
+	int Receive(char* msg, unsigned int len, unsigned int timeout);
+	/*
+	*发送信息
+	*/
+	int Send(std::string &msg, unsigned int timeout);
+	/*
+	*发送信息
+	*/
+	int Send(const char* msg, unsigned int len, unsigned int timeout);
+
+	~SocketClientEx();
+
+private:
+	SOCKETex_t handle;
+	fd_set fs;
+	bool isconnected;
+
+protected:
+
+};
+
