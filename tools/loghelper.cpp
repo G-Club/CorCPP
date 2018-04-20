7b36f27d1dc1bbfc2a3cc8882238ee31300046cd
YoyaoFeng <fwxshiwo@gmail.com>
2018-04-20 23:12:50 +0800

    cross platform log tool

--- tools/loghelper.cpp
+++ tools/loghelper.cpp
@@ -1,9 +1,18 @@
-
+#define _CRT_SECURE_NO_WARNINGS
 #include "loghelper.h"
-#include <direct.h>
+#include <limits.h>
+#ifdef __linux__
+#include <unistd.h>
+#endif
 
-int LogHelper::DEBUG_MOD=0;
-std::string LogHelper::logname="ralog.log";
+#ifdef _WIN32
+#include <direct.h>
+#endif
+
+#include <string.h>
+
+
+int LogHelper::DEBUG_MOD = 0;
 
 LogHelper::LogHelper()
 {
@@ -15,23 +24,40 @@ LogHelper::~LogHelper()
 }
 
 
-void LogHelper::writeLog(LogLevel lev, const char *format, ...)
+static std::string GetCurPath()
 {
-	int count = 0;
-	va_list	arg;
-	va_start(arg, format);
-	read_writeLog(lev,format, arg);
-	va_end(arg);
-	
+	std::string path = "";
+#ifdef __linux__
+
+	char *path_name = get_current_dir_name();
+	path = path_name;
+
+#endif
+
+#ifdef _WIN32
+	char chpath[_MAX_PATH] = { 0 };
+	getcwd(chpath, sizeof(chpath));
+	path = chpath;
+
+#endif
+
+	return path;
 }
 
-#define MAX_PATH 266
-
-void LogHelper::read_writeLog(LogLevel lev, const char *format, va_list arg)
+void LogHelper::writeLog(LogLevel lev, const char *format, ...)
 {
-	char pbString[1024]={0};
+	va_list	arg;
+	va_start(arg, format);
+	writeLog(lev, format, arg);
+	va_end(arg);
 
-	vsprintf_s(pbString, format, arg);
+}
+
+void LogHelper::writeLog(LogLevel lev, const char *format, va_list arg)
+{
+	char pbString[1024] = { 0 };
+
+	vsprintf(pbString, format, arg);
 
 #if DEBUG_MOD
 
@@ -40,41 +66,36 @@ void LogHelper::read_writeLog(LogLevel lev, const char *format, va_list arg)
 	return;
 
 #else 
-	
+
 	std::string CurrentPath;
-	char chpath[MAX_PATH] = { 0 };
-	getcwd(chpath, sizeof(chpath));
-	CurrentPath = chpath;
-	//int index = CurrentPath.rfind('\\');
-	//CurrentPath = CurrentPath.substr(0, index + 1);
-	CurrentPath.append("/");
-	CurrentPath += logname;
+	CurrentPath = GetCurPath();
+	CurrentPath += "/mylog.log";
 
 	FILE *file = NULL;
-	fopen_s(&file, CurrentPath.c_str(), "ab");
+	file = fopen(CurrentPath.c_str(), "ab");
 	if (file == NULL)
 	{
-		fopen_s(&file, CurrentPath.c_str(), "ab");
+		file = fopen(CurrentPath.c_str(), "ab");
 	}
-	int count = 0;
 
 	char timestr[64] = { 0 };
-	const char *levstr="[INFO]";
-	if(lev==LogLevel::L_ERROR)
+	const char *levstr = "[INFO]";
+	if (lev == LogLevel::L_ERROR)
 	{
-		levstr="[ERROR]";
+		levstr = "[ERROR]";
 	}
 	if (lev == LogLevel::L_WARNING)
 	{
-		levstr="[WARNING]";
+		levstr = "[WARNING]";
 	}
 	FormattTime(time(NULL), timestr, sizeof(timestr), NULL);
-	strcat_s(timestr, " ");
-	strcat_s(timestr,levstr);
-	strcat_s(timestr, " ");
+	strcat(timestr, " ");
+	strcat(timestr, levstr);
+	strcat(timestr, " ");
 	fwrite(timestr, strlen(timestr), 1, file);
+
 	//参数的值不能正常显示，原因是参数传递错误，fprintf不能正确处理va_list类型的参数
-	count = fprintf(file, "%s\n",pbString);
+	fprintf(file, "%s\n", pbString);
 
 	fflush(file);
 	fclose(file);
@@ -93,7 +114,8 @@ void LogHelper::FormattTime(time_t t, char *formstr, size_t size, const char*for
 	{
 		formatt = "%Y-%m-%d %H:%M:%S";
 	}
-	struct tm ptm ;
-	localtime_s(&ptm,&t);
-	strftime(formstr, size, formatt, &ptm);
+	struct tm *ptm;
+	ptm = localtime(&t);
+
+	strftime(formstr, size, formatt, ptm);
 }
