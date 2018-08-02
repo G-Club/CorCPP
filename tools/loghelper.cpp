d503b1bcce99a0c3ab745c77e0bd8856d131849e
YoyaoFeng <fwxshiwo@gmail.com>
2018-08-02 17:44:50 +0800

    localtime改为了localtime_r

--- tools/loghelper.cpp
+++ tools/loghelper.cpp
@@ -36,51 +36,16 @@ static std::string GetCurPath()
 
 #endif
 }
-/*
-void LogHelper::writeLog(LogLevel lev, const char *format, ...)
+
+void LogHelper::writeLog(bool isdebug,const char *timestr, const char *logstr)
 {
-    va_list	arg;
-    va_start(arg, format);
-    writeLog(lev,format, arg);
-    va_end(arg);
-
-}
-*/
-static const char *lvstr[]={
-    "[INFO]",
-    "[WARNING]",
-    "[ERROR]"
-};
-
-void LogHelper::writeLog(LogLevel lev,bool isdebug, const char *format, ...)
-{
-    char pbString[1024]={0};
-    va_list	arg;
-    va_start(arg, format);
-    vsprintf(pbString, format, arg);
-    va_end(arg);
-
-    char timestr[256] = { 0 };
-    const char *levstr=lvstr[lev];
-    GetTimestamp(timestr,sizeof(timestr));
-    strcat(timestr, " ");
-    strcat(timestr,levstr);
-
+    std::lock_guard<std::mutex> locker(lock);
     if (isdebug)
     {
-        std::lock_guard<std::mutex> locker(lock);
-        std::cout<<timestr<<" "<<pbString << std::endl;
+        std::cout<<timestr<<" "<<logstr << std::endl;
         return;
     }
-    /*
-    try
-    {
-        std::call_once(flag,Init);
-    }catch (std::exception &ex)
-    {
-        std::cout<<"exception:"<<ex.what()<<std::endl;
-    }
-    */
+
     Init();
     if (file == NULL)
     {
@@ -89,63 +54,62 @@ void LogHelper::writeLog(LogLevel lev,bool isdebug, const char *format, ...)
     }
     // fwrite(timestr, strlen(timestr), 1, file);
     //参数的值不能正常显示，原因是参数传递错误，fprintf不能正确处理va_list类型的参数
-    std::lock_guard<std::mutex> locker(lock);
-    fprintf(file, "%s %s\n",timestr,pbString);
+    fprintf(file, "%s %s\n",timestr,logstr);
     fflush(file);
     fclose(file);
     file=NULL;
 }
 
-
-#define MAX_PATH 266
-
-void LogHelper::writeLog(LogLevel lev, bool isdebug, const char *format, va_list &arg)
+void LogHelper::Error(bool isdebug,const char *format,...)
 {
-    char pbString[1024]={0};
-    FILE *file = NULL;
-
+    char pbString[2048]={0};
+    va_list	arg;
+    va_start(arg, format);
     vsprintf(pbString, format, arg);
+    va_end(arg);
 
-    std::string CurrentPath;
-    CurrentPath = GetCurPath();
-    CurrentPath += "/mylog.log";
-
-    char timestr[64] = { 0 };
-    const char *levstr="[INFO]";
-    if(lev==LogLevel::L_ERROR)
-    {
-        levstr="[ERROR]";
-    }
-    if (lev == LogLevel::L_WARNING)
-    {
-        levstr="[WARNING]";
-    }
-    FormattTime(time(NULL), timestr, sizeof(timestr), NULL);
+    char timestr[256] = { 0 };
+    const char *levstr="[ERROR]";
+    GetTimestamp(timestr,sizeof(timestr));
     strcat(timestr, " ");
     strcat(timestr,levstr);
-    strcat(timestr, " ");
-    fwrite(timestr, strlen(timestr), 1, file);
 
-    if (isdebug)
-    {
-        std::cout << pbString << std::endl;
-
-        return;
-    }
-
-    file = fopen( CurrentPath.c_str(), "ab");
-    if (file == NULL)
-    {
-        file =  fopen(CurrentPath.c_str(), "ab");
-    }
-
-    //参数的值不能正常显示，原因是参数传递错误，fprintf不能正确处理va_list类型的参数
-    fprintf(file, "%s\n",pbString);
-
-    fflush(file);
-    fclose(file);
-    file=NULL;
+    writeLog(isdebug,timestr,pbString);
 }
+
+void LogHelper::Info(bool isdebug, const char *format,...)
+{
+    char pbString[2048]={0};
+    va_list	arg;
+    va_start(arg, format);
+    vsprintf(pbString, format, arg);
+    va_end(arg);
+
+    char timestr[256] = { 0 };
+    const char *levstr="[INFO]";
+    GetTimestamp(timestr,sizeof(timestr));
+    strcat(timestr, " ");
+    strcat(timestr,levstr);
+
+    writeLog(isdebug,timestr,pbString);
+}
+
+void LogHelper::Warning(bool isdebug,const char *format,...)
+{
+    char pbString[2048]={0};
+    va_list	arg;
+    va_start(arg, format);
+    vsprintf(pbString, format, arg);
+    va_end(arg);
+
+    char timestr[256] = { 0 };
+    const char *levstr="[WARNING]";
+    GetTimestamp(timestr,sizeof(timestr));
+    strcat(timestr, " ");
+    strcat(timestr,levstr);
+    writeLog(isdebug,timestr,pbString);
+}
+
 
 void LogHelper::FormattTime(time_t t, char *formstr, size_t size, const char*formatt)
 {
@@ -157,8 +121,14 @@ void LogHelper::FormattTime(time_t t, char *formstr, size_t size, const char*for
     {
         formatt = "%Y-%m-%d %H:%M:%S";
     }
-    struct tm *ptm ;
-    ptm= localtime(&t);
+    struct tm *ptm =NULL;
+    /*
+当我们使用localtime(timestamp)的时候返回是一个指针。
+这个指针的指向是共用的，这时如果有其他线程执行了localtime。。。。会覆盖之前的值！！
+可以考虑localtime_r
+*/
+    ptm=localtime_r(&t,ptm);
+
     strftime(formstr, size, formatt, ptm);
 }
 
